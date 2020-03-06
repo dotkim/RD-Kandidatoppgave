148f87342c7119dfbbe79c3838c4faf6f7fce376
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2020-03-06 23:04:24 +0100

    Created POCOs for the Person and Enterprise types

---
+++ WebApplication1/Enterprise.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Enterprise
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+
+        [StringLength(StringLengthAttribute.MaxText)]
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication1/Person.cs
@@ -0,0 +1,17 @@
+﻿using ServiceStack.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Person
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        [StringLength(StringLengthAttribute.MaxText)]
+        public string Name { get; set; }
+
+        [Reference]
+        public Enterprise Enterprise { get; set; }
+    }
+}
