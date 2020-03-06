afea2aa923c172239752ae5fb24514c0a3f7c9cf
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2020-03-06 23:16:22 +0100

    Basic routing and reuse old Encoding.

---
+++ WebApplication1/PeopleRequest.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack;
+using System.Collections.Generic;
+
+namespace WebApplication1
+{
+    [Route("/people", "GET")]
+    public class GetPeople : IReturn<List<Person>>
+    {
+    }
+
+    [Route("/people/{Id}", "GET")]
+    public class SearchPeople : IReturn<Person>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ WebApplication1/PeopleService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ServiceStack;
+using ServiceStack.Data;
+
+namespace WebApplication1
+{
+    public class PeopleService : Service
+    {
+        private readonly IDatabase _database;
+        private readonly IDbConnectionFactory _dbFactory;
+
+        public PeopleService(IDatabase database, IDbConnectionFactory dbFactory)
+        {
+            _database = database;
+            _dbFactory = dbFactory;
+        }
+
+        public List<Person> Get(GetPeople request)
+        {
+            var result = _database.ExampleData(_dbFactory);
+            return result;
+        }
+
+        public Person Get(SearchPeople request)
+        {
+            var result = _database.LoadPersonById(_dbFactory, request.Id);
+            return result;
+        }
+    }
+}
