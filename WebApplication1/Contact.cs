64c8f0d9dec3ce49d0318d9735aa8a8b1f7cab31
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2020-03-07 21:43:02 +0100

    Added Contact POCOs

---
+++ WebApplication1/Contact.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Contact
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        [ForeignKey(typeof(Person))]
+        public int PersonId { get; set; }
+
+        [ForeignKey(typeof(Person))]
+        public int ContactPersonId { get; set; }
+    }
+
+    [Alias("Contact")]
+    public class ContactFull
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        [References(typeof(Person))]
+        public string PersonName { get; set; }
+
+        [References(typeof(Person))]
+        public string ContactPersonName { get; set; }
+    }
+}
